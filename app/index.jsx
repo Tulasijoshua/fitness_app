396c93f68562d913242eac76bbb3d6de7c367fb9
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-18 20:21:25 +0000

    Feat: home page

--- app/index.jsx
+++ app/index.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
+import Animated, { FadeIn, FadeInDown, FadeOut } from 'react-native-reanimated';
+import { useRouter } from 'expo-router';
+
 
 
 export default function index() {
+    const router = useRouter();
   return (
     <View className="flex-1 flex justify-end">
       <StatusBar style="light" />
@@ -18,17 +22,18 @@ export default function index() {
         end={{x: 0.5, y: 0.8}}
         className="flex justify-end pb-12 space-y-8"
         >
-            <View className="flex items-center">
+            <Animated.View entering={FadeInDown.delay(100).springify()} className="flex items-center">
                 <Text style={{fontSize: hp(5)}} className="text-white font-bold tracking-wide">
                     Best <Text className="text-rose-500">Workouts</Text> 
                 </Text>
                 <Text style={{fontSize: hp(5)}} className="text-white font-bold tracking-wide">
                     For you
                 </Text>
-            </View>
+            </Animated.View>
 
-            <View>
+            <Animated.View entering={FadeInDown.delay(200).springify()}>
                 <TouchableOpacity
+                    onPress={() => router.push('home')}
                     style={{height: hp(7), width: wp(80)}}
                     className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
                 >
@@ -36,7 +41,7 @@ export default function index() {
                         Get Started
                     </Text>
                 </TouchableOpacity>
-            </View>
+            </Animated.View>
         </LinearGradient>
     </View>
   )
