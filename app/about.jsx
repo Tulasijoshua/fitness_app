8a4b4a1daa3abee9b96cf556f8f86c1a0aff27ae
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-18 18:32:22 +0000

    startup files

---
+++ app/about.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function about() {
+  return (
+    <View>
+      <Text>This is about page</Text>
+    </View>
+  )
+}
