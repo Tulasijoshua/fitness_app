aab66e923e776d9baf5adc7774bb6b1e25543c32
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-18 19:01:57 +0000

    Feat: Welcome screen started

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { StatusBar } from 'expo-status-bar';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>our first screen</Text>
+    <View className="flex-1 flex justify-end">
+      <StatusBar style="light" />
+      <Image className="h-full w-full absolute" source={require('../assets/images/welcome.png')} />
+      
+      <LinearGradient
+        colors={['transparent', '#18181b']}
+        style={{width: wp(100), height: hp(70)}}
+        start={{x: 0.5, y: 0}}
+        end={{x: 0.5, y: 0.8}}
+        className="flex justify-end pb-12 space-y-8"
+        >
+            <View className="flex items-center">
+                <Text style={{fontSize: hp(5)}} className="text-white">
+                    Best <Text className="text-rose-500">Workouts</Text> 
+                </Text>
+            </View>
+        </LinearGradient>
     </View>
   )
 }
