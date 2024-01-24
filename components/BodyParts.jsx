5263ee0cc64979bd38765cce9964f1ca40261145
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-24 22:38:42 +0000

    Feat: update

--- components/BodyParts.jsx
+++ components/BodyParts.jsx
@@ -1,6 +1,7 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { bodyParts } from '../constants';
 
 
 export default function BodyParts() {
@@ -9,6 +10,24 @@ export default function BodyParts() {
       <Text style={{fontSize: hp(3)}} className="font-semibold  text-neutral-700">
         Exercises
       </Text>
+
+      <FlatList 
+        data={bodyParts}
+        numColumns={2}
+        keyExtractor={item => item.name}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 50, paddingTop: 20}}
+        columnWrapperStyle={{
+            justifyContent: 'space-between'
+        }}
+        renderItem={({item, index}) => <BodyPartCard index={index} item={item}/>}
+      />
     </View>
   )
+}
+
+const BodyPartCard = ({item, index}) => {
+    return (
+        <Text>Item</Text>
+    )
 }
