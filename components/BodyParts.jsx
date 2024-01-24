6a684d8f04a78f6759de115bed4970d402c2f203
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-24 22:54:50 +0000

    Feat: body parts

--- components/BodyParts.jsx
+++ components/BodyParts.jsx
@@ -2,6 +2,7 @@ import { View, Text, FlatList, Touchable, TouchableOpacity, Image } from 'react-
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { bodyParts } from '../constants';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 export default function BodyParts() {
@@ -36,7 +37,21 @@ const BodyPartCard = ({item, index}) => {
                         source={item.image} 
                         resizeMode='cover'
                         style={{width: wp(44), height: wp(52)}}
+                        className="rounded-[35px] absolute"
                     />
+                    <LinearGradient
+                        colors={['transparent', 'rgba(0, 0, 0, 0.9)']}
+                        style={{width: wp(44), height: hp(15)}}
+                        start={{x: 0.5, y: 0}}
+                        end={{x: 0.5, y: 1}}
+                        className="absolute bottom-0 rounded-b-[35px]"
+                    />
+                    <Text 
+                        style={{fontSize: hp(2.3)}}
+                        className="text-white font-semibold text-center tracking-wide"
+                    >
+                        {item.name}
+                    </Text>
                 </TouchableOpacity>
         </View>
     )
