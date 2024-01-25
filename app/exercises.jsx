2b69d2514ad30c6d4aa4bc8436c0b987edb4eef6
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-25 21:05:47 +0000

    Feat: exercises

--- app/exercises.jsx
+++ app/exercises.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 
 export default function Exercises() {
+  const router = useRouter();
+  const item = useLocalSearchParams();
+  console.log('got item: ', item)
+
   return (
-    <View>
-      <Text>exercises</Text>
+    <View className="mt-20">
+      <Text>Exercises</Text>
+      <TouchableOpacity onPress={() => router.back()}>
+        <Text>Go back</Text>
+      </TouchableOpacity>
     </View>
   )
 }
