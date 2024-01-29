807995989c4a202fe44fc4d63da8988c3107823d
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-29 22:17:07 +0000

    Feat: update

--- app/exerciseDetails.jsx
+++ app/exerciseDetails.jsx
@@ -1,8 +1,9 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { Image } from 'expo-image';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 
 export default function exerciseDetails() {
@@ -22,10 +23,33 @@ export default function exerciseDetails() {
       </View>
       <TouchableOpacity
         onPress={() => router.back()}
-        className="mx-2 absolute rounded-full mt-2 right-0"
+        className="mx-2 absolute rounded-full mt-8 right-4"
       >
-        
+        <AntDesign name="closecircle" size={hp(3)} color="#f43f5e" />
       </TouchableOpacity>
+
+      {/* details */}
+      <ScrollView className="mx-4 space-y-2 mt-3">
+        <Text style={{fontSize: hp(3.5)}}
+          className="font-semibold text-neutral-800 tracking-wide"
+        >
+          {item.name}
+        </Text>
+        <Text style={{fontSize: hp(2)}}
+          className="text-neutral-800 tracking-wide"
+        >
+          Equipment <Text className="font-bold text-neutral-800">
+            {item?.equipment}
+          </Text>
+        </Text>
+        <Text style={{fontSize: hp(2)}}
+          className="text-neutral-800 tracking-wide"
+        >
+          Secondary Muscles <Text className="font-bold text-neutral-800">
+            {item?.secondaryMuscles}
+          </Text>
+        </Text>
+      </ScrollView>
     </View>
   )
 }
