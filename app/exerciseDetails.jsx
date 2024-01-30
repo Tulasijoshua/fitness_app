f3569eca99ab8768c3103fe7dead22e4c7eeed77
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-30 18:01:55 +0000

    Feat: exercise details animation

--- app/exerciseDetails.jsx
+++ app/exerciseDetails.jsx
@@ -4,6 +4,7 @@ import { useLocalSearchParams, useRouter } from 'expo-router';
 import { Image } from 'expo-image';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Animated, { FadeInDown } from 'react-native-reanimated';
 
 
 export default function exerciseDetails() {
@@ -30,47 +31,58 @@ export default function exerciseDetails() {
 
       {/* details */}
       <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 60}} className="mx-4 space-y-2 mt-3">
-        <Text style={{fontSize: hp(3.5)}}
+        <Animated.Text
+          entering={FadeInDown.duration(300).springify()}
+          style={{fontSize: hp(3.5)}}
           className="font-semibold text-neutral-800 tracking-wide"
         >
           {item.name}
-        </Text>
-        <Text style={{fontSize: hp(2)}}
+        </Animated.Text>
+        <Animated.Text
+          entering={FadeInDown.delay(100).duration(300).springify()} 
+          style={{fontSize: hp(2)}}
           className="text-neutral-800 tracking-wide"
         >
           Equipment <Text className="font-bold text-neutral-800">
             {item?.equipment}
           </Text>
-        </Text>
-        <Text style={{fontSize: hp(2)}}
+        </Animated.Text>
+        <Animated.Text
+          entering={FadeInDown.delay(200).duration(300).springify()}  
+          style={{fontSize: hp(2)}}
           className="text-neutral-800 tracking-wide"
         >
           Secondary Muscles <Text className="font-bold text-neutral-800">
             {item?.secondaryMuscles}
           </Text>
-        </Text>
-        <Text style={{fontSize: hp(2)}}
+        </Animated.Text>
+        <Animated.Text
+          entering={FadeInDown.delay(300).duration(300).springify()}  
+          style={{fontSize: hp(2)}}
           className="text-neutral-800 tracking-wide"
         >
           Target <Text className="font-bold text-neutral-800">
             {item?.target}
           </Text>
-        </Text>
-        <Text style={{fontSize: hp(3)}}
+        </Animated.Text>
+        <Animated.Text
+          entering={FadeInDown.delay(400).duration(300).springify()}  
+          style={{fontSize: hp(3)}}
           className="font-semibold text-neutral-800 tracking-wide"
         >
           Instructions
-        </Text>
+        </Animated.Text>
         {
           item.instructions?.split(',').map((instruction, index) => {
             return (
-              <Text
+              <Animated.Text
+                entering={FadeInDown.delay(500).duration(300).springify()} 
                 key={instruction}
                 style={{fontSize: hp(1.7)}}
                 className="text-neutral-800"
               >
                 {instruction}
-              </Text>
+              </Animated.Text>
             )
           })
         }
