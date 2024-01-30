3b4ca6211ee73730640c55660570fcec08d52f61
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-30 17:45:03 +0000

    Feat: adding animation

--- app/exerciseDetails.jsx
+++ app/exerciseDetails.jsx
@@ -12,7 +12,7 @@ export default function exerciseDetails() {
 
     console.log('got item: ', item)
   return (
-    <View class="flex flex-1">
+    <View class="flex flex-1 pt-[7rem]">
       <View class="shadow-md bg-neutral-200 rounded-b-[40px]">
         <Image
           source={{uri: item.gifUrl}}
@@ -22,14 +22,14 @@ export default function exerciseDetails() {
         />
       </View>
       <TouchableOpacity
-        onPress={() => router.back()}
-        className="mx-2 absolute rounded-full mt-8 right-4"
+        onPress={() => {router.back()}}
+        className="mx-2 absolute rounded-full top-10 right-4"
       >
         <AntDesign name="closecircle" size={hp(3)} color="#f43f5e" />
       </TouchableOpacity>
 
       {/* details */}
-      <ScrollView className="mx-4 space-y-2 mt-3">
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 60}} className="mx-4 space-y-2 mt-3">
         <Text style={{fontSize: hp(3.5)}}
           className="font-semibold text-neutral-800 tracking-wide"
         >
@@ -56,6 +56,24 @@ export default function exerciseDetails() {
             {item?.target}
           </Text>
         </Text>
+        <Text style={{fontSize: hp(3)}}
+          className="font-semibold text-neutral-800 tracking-wide"
+        >
+          Instructions
+        </Text>
+        {
+          item.instructions?.split(',').map((instruction, index) => {
+            return (
+              <Text
+                key={instruction}
+                style={{fontSize: hp(1.7)}}
+                className="text-neutral-800"
+              >
+                {instruction}
+              </Text>
+            )
+          })
+        }
       </ScrollView>
     </View>
   )
